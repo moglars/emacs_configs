bb785db2de5cdcd3be81a09c7fa2b915177fdef4
Marc B <marc.brueni@hispeed.ch>
2017-09-24 14:02:36 +0200

    add config change

--- emacs.el
+++ emacs.el
@@ -62,11 +62,57 @@ with -Wall and -ansi options. Warnings are shown in the compile output."
          (comint-send-input)
          ))
 
+(defun compile-run-c-2 ()
+  "Sets up a frame layout with 3 windows. Frame is vertially split into code
+and run area. The run area on the right is horizontally split into
+compile output and shell that runs the code. The current buffer is compiled
+with -Wall and -ansi options. Warnings are shown in the compile output."
+  (interactive)
+  (save-buffer)
+  (let* ((src-file-name (buffer-file-name))
+         (src-dir-name (file-name-directory src-file-name))
+         (src-buffer (current-buffer))
+         (compile-output-buffer (get-buffer-create "*compile-output2*"))
+         (shell-buffer (get-buffer-create "run-shell"))
+         (compile-result))
+    (delete-other-windows)
+    (split-window-right)
+    (other-window 1)
+    (switch-to-buffer compile-output-buffer)
+    (setq buffer-read-only nil)
+    (delete-region (point-min) (point-max))
+    (set-buffer src-buffer)
+    (setq default-directory src-dir-name)
+    (save-restriction
+      (widen)
+      ;;I don't know yet, how to inform the compilation mode, in which buffer it should search the next error.
+      ;;(setq compile-result (call-process-region (point-min) (point-max) "gcc" nil compile-output-buffer t "-Wall" "-ansi" "-xc" "-")))
+      (setq compile-result (call-process "gcc" nil compile-output-buffer t "-Wall" "-ansi" src-file-name)))
+    (set-buffer compile-output-buffer)
+    (compilation-mode)
+    (setq buffer-read-only t)
+    (split-window-below)
+    (other-window 1)
+    (switch-to-buffer "run-shell")
+    (shell (current-buffer))
+    (sit-for 0.01)
+    (comint-interrupt-subjob)
+    (sit-for 0.01)
+    (insert (format "cd %s" src-dir-name))
+    (comint-send-input)
+    (sit-for 0.01)
+    (if (= 0 compile-result)
+        (progn
+          (insert "./a.out")
+          (comint-send-input))
+      (insert "echo compile-error")
+      (comint-send-input))
+    ))
+
 (defun compile-test()
   (interactive)
   (save-restriction
-    (message "min %d, max %d" (point-min) (point-max))
-    (let ((compile-result  (call-process-region 0 1 "gcc" nil "*compile-output*" t "-Wall" "-ansi" "-xc" "-")))
+    (let ((compile-result  (call-process-region (point-min) (point-max) "gcc" nil "*test-compile-output*" t "-Wall" "-ansi" "-xc" "-")))
       (message "compile result is %d" compile-result))))
   
 (global-set-key (kbd "C-c b") 'switch-to-prev-buffer)
@@ -88,7 +134,7 @@ with -Wall and -ansi options. Warnings are shown in the compile output."
 (menu-bar-mode 0)
 
 (global-set-key [f2] 'compile-run-c)
-(global-set-key [f3] 'compile-test)
+(global-set-key [f3] 'compile-run-c-2)
 (add-hook 'shell-mode
           (lambda () (local-set-key (kbd "C-c C-d") 'comint-interrupt-subjob)))
 
